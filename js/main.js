b7718f0b08b036dfcbe51698bd53b4495d1f158c
Tim Charlier <timcharlier6@etik.com>
2024-08-08 14:58:55 +0200

    feat: melodyToQwerty and ProcessUserInput

--- js/main.js
+++ js/main.js
@@ -1,46 +1,64 @@
-// Render a new sentence on the screen
+const melody = [
+  "a",
+  "a",
+  "j",
+  "j",
+  "k",
+  "k",
+  "j",
+  "␣",
+  "f",
+  "f",
+  "d",
+  "d",
+  "s",
+  "s",
+  "a",
+];
+
+function getNextMelody() {
+  return melody.slice();
+}
+
 function renderNewSentence(melody) {
   const sentenceDisplayElement = document.getElementById("sentenceDisplay");
   sentenceDisplayElement.innerHTML = "";
-  melody.split("").forEach((character) => {
+  melody.push("↵");
+  melody.forEach((character) => {
     const characterSpan = document.createElement("span");
     characterSpan.innerText = character;
     sentenceDisplayElement.appendChild(characterSpan);
   });
 }
 
-// Process user typing input
 function processUserTypingInput() {
   const sentenceDisplayElement = document.getElementById("sentenceDisplay");
   const inputElement = document.getElementById("typeInput");
 
-  // Generate the initial sentence default
-  let randomSentence = generateRandomSentence();
-  renderNewSentence(randomSentence);
+  let nextMelody = getNextMelody();
+  renderNewSentence(nextMelody);
 
-  // Event listener for user input
   inputElement.addEventListener("input", () => {
+    let arrayValue = inputElement.value.split("");
+
+    if (arrayValue[arrayValue.length - 1] === " ") {
+      arrayValue[arrayValue.length - 1] = "␣";
+    } else if (arrayValue[arrayValue.length - 1] === "\n") {
+      arrayValue[arrayValue.length - 1] = "↵";
+    }
+
+    inputElement.value = arrayValue.join("");
+
     const arraySentence = sentenceDisplayElement.querySelectorAll("span");
-    const arrayValue = inputElement.value.split("");
-    console.log(arrayValue);
 
     let correct = true;
     arraySentence.forEach((characterSpan, index) => {
       const character = arrayValue[index];
       if (character == null) {
-        characterSpan.classList.remove("correct");
-        characterSpan.classList.remove("incorrect");
-        characterSpan.classList.remove("underline");
+        characterSpan.classList.remove("correct", "incorrect", "underline");
       } else if (character === characterSpan.innerText) {
         characterSpan.classList.add("correct");
-        characterSpan.classList.remove("incorrect");
-        characterSpan.classList.remove("underline");
-      } else if (
-        character !== characterSpan.innerText &&
-        (characterSpan.innerText === " " || characterSpan.innerText === "\n")
-      ) {
-        characterSpan.classList.add("incorrect");
-        correct = false;
+        characterSpan.classList.remove("incorrect", "underline");
       } else {
         characterSpan.classList.remove("correct");
         characterSpan.classList.add("incorrect");
@@ -53,16 +71,12 @@ function processUserTypingInput() {
       arraySentence[arrayValue.length].classList.add("underline");
     }
 
-    if (arrayValue.length === arraySentence.length) {
-      if (correct) {
-        // Generate a new sentence based on current difficulty level
-        const newDifficulty = "medium"; // Change difficulty as needed
-        randomSentence = generateRandomSentence(newDifficulty, 8);
-        renderNewSentence(randomSentence);
-        inputElement.value = ""; // Clear the input field
-      } else {
-        inputElement.value = ""; // Clear the input field on incorrect completion
-      }
+    if (arrayValue.length === arraySentence.length && correct) {
+      nextMelody = getNextMelody();
+      renderNewSentence(nextMelody);
+      inputElement.value = "";
     }
   });
 }
+
+processUserTypingInput();
